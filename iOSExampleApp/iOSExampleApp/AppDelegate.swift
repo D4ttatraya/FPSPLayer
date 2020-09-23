f0eed82c8eddbd21a7980a4116ef38d9a51e3c97
d4ttatraya <d4ttatraya@gmail.com>
2020-09-23 21:31:55 +0530

    iOSExampleApp initial commit

---
+++ iOSExampleApp/iOSExampleApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iOSExampleApp
+//
+//  Created by D4ttatraya on 23/09/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
