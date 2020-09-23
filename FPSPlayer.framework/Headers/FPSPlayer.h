56b95db1499f4628f8c6dbedee5cd70d02551836
Dattatraya Anarase <d4ttatraya@gmail.com>
2020-09-23 19:02:28 +0530

    Initial Player

---
+++ FPSPlayer.framework/Headers/FPSPlayer.h
@@ -0,0 +1,19 @@
+//
+//  FPSPlayer.h
+//  FPSPlayer
+//
+//  Created by D4ttatraya on 16/04/20.
+//  Copyright © 2020 D4ttatraya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FPSPlayer.
+FOUNDATION_EXPORT double FPSPlayerVersionNumber;
+
+//! Project version string for FPSPlayer.
+FOUNDATION_EXPORT const unsigned char FPSPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FPSPlayer/PublicHeader.h>
+
+
